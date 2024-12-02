c21e65d730669411f0e283c56fa65c84af0e8189
priyansh.mali <priyansh.mali@piramal.com>
2024-12-02 22:46:21 +0530

    test

--- FeatureTest.java
+++ FeatureTest.java
@@ -1,1 +1,11 @@
-public class FeatureTest {/* TODO: example */}
+public class FeatureTest {
+    /* TODO: example */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+
+    public static void test() {
+        System.out.println("Hello, World!");
+    }
+    /* TODO: example */
+}
