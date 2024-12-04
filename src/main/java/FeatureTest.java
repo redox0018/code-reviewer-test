7a73f4804b043c7977cf218330c21f298be93aa3
priyansh.mali <priyansh.mali@piramal.com>
2024-12-04 16:10:00 +0530

    test

--- src/main/java/FeatureTest.java
+++ src/main/java/FeatureTest.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 public class FeatureTest {
     public static void main(String[] args) {
         log.info("Hello, World!");
+        log.info("More");
     }
 
     public int square(int variable){
